6bed260375b6930fa881278522c945cbb911dea7
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-08-25 11:46:25 +0000

    seco

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints a specific string using puts
+ *
  * Return: 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,\n");
+	puts("Programming is like building a multilingual puzzle,\n");
 	return (0);
 }
