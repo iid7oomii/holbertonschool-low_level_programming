8471dc52aef53bf6c6b6d911af4fcd41bdac9e7a
root <root@ip-10-42-211-177.ec2.internal>
2025-08-26 12:27:24 +0000

    seco

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,11 +13,11 @@ char mychar;
 long int mylongint;
 long long int mylonglongint;
 float myfloat;
-	printf("Size of char: %zu byte(s)\n", sizeof(mychar));
-	printf("Size of int: %zu byte(s)\n", sizeof(myint));
-	printf("Size of long int: %ld byte(s)\n", sizeof(mylongint));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(mylonglongint));
-	printf("Size of float: %zu byte(s)\n", sizeof(myfloat));
+	printf("Size of a char: %zu byte(s)\n", sizeof(mychar));
+	printf("Size of an int: %zu byte(s)\n", sizeof(myint));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(mylongint));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(mylonglongint));
+	printf("Size of a float: %zu byte(s)\n", sizeof(myfloat));
 	return (0);
 
 }
