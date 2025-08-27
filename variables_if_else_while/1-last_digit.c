f7f14c17ed230ac8c823b5d0478523df82baf36c
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-08-27 13:52:33 +0000

    secoo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,8 +14,8 @@ int main (void)
 	int last_digit;
 
 	srand(time(0));
-	n=rand() - RRAND_MAX / 2;
-	last-digit = n % 10;
+	n=rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
 	if (last_digit > 5)
 {
@@ -27,6 +27,7 @@ int main (void)
 }
 	else {
 		printf("and is less than 6 and not 0\n");
+	}
 
 		return (0);
 }
