086be8d64c98863a4081483bbc0ea5ba9dc78423
iid7oomii <abdulrahman18alghamdi>
2025-08-27 21:10:38 +0000

    secc

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lwercase
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for ( c= 'a'; c <= 'z'; c++)
+
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
