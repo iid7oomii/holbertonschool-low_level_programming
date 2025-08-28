e8ccd14230c3ad3b2a42223ea9fb184d55e6e4b1
iid7oomii <abdulrahman18alghamdi>
2025-08-28 00:02:04 +0000

    secc

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
