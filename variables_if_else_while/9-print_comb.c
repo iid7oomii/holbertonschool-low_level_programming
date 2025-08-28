3c8382615d7eaf9702bc116141855a80a5400c87
iid7oomii <abdulrahman18alghamdi>
2025-08-28 01:39:00 +0000

    secc

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+
+	if (n != '9')
+	
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
