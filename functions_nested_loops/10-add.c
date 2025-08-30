2525250bfe3609c17754573e7abfaaf29aff2122
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-08-30 19:58:24 +0000

    see

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,15 +2,13 @@
 
 /**
  * add - adds two values
- * @1: The first number to be computed
- * @2: The second number to be computed
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
  * Return: Integer value
  */
 
 int add(int n1, int n2)
 
 {
-
-	return(n1 + n2);
-
+	return (n1 + n2);
 }
