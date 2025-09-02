59792954510012414823051dae7eea1ab346d1f2
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-09-02 17:07:01 +0000

    sds

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+
+{
+	int l, n;
+
+	for (l = 0; l < 10; l++)
+	{
+	for (n = 0; n <= 14; n++)
+	{
+	if(n >= 10)
+
+	_putchar('0' + n / 10);
+
+	_putchar('0' + n % 10);
+
+	}
+
+	_putchar('\n');
+	}
+}
