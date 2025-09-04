4dc1e648eb7fe5a5d9e9a13a3a56c53984d28039
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-09-04 16:48:00 +0000

    ses

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string to process
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start, i;
+
+
+	while (str[length] != '\0')
+	{
+	length++;
+	}
+
+
+	if (length % 2 == 0)
+	start = length / 2;
+	else
+	start = (length + 1) / 2;
+
+
+	for (i = start; i < length; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
