4b6671520163e7822aeb97c23738818229de6f4e
root <abdulrahman18alghamdi@gmail.com  git config --global user.name iid7oomii>
2025-09-06 16:43:55 +0000

    ses

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates the string pointed to by src
+ *            to the end of the string pointed to by dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	i++;
+
+	while (src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
