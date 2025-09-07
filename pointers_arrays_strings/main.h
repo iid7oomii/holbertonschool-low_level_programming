a4e21429a78ae6f67ddb5f97e3ca1191086c1af0
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-09-07 22:02:31 +0000

    test

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,37 +1,17 @@
 int _putchar(char c);
 
-void reset_to_98(int *n);
+char *_memset(char *s, char b, unsigned int n);
 
-void swap_int(int *a, int *b);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
-int _strlen(char *s);
+char *_strchr(char *s, char c);
 
-void _puts(char *str);
+unsigned int _strspn(char *s, char *accept);
 
-void print_rev(char *s);
+char *_strpbrk(char *s, char *accept);
 
-void rev_string(char *s);
+char *_strstr(char *haystack, char *needle);
 
-void puts2(char *str);
+void print_chessboard(char (*a)[8]);
 
-void puts_half(char *str);
-
-void print_array(int *a, int n);
-
-char *_strcpy(char *dest, char *src);
-
-int _atoi(char *s);
-
-char *_strcat(char *dest, char *src);
-
-char *_strncat(char *dest, char *src, int n);
-
-char *_strncpy(char *dest, char *src, int n);
-
-int _strcmp(char *s1, char *s2);
-
-void reverse_array(int *a, int n);
-
-char *string_toupper(char *);
-
-char *cap_string(char *);
+void print_diagsums(int *a, int size);
