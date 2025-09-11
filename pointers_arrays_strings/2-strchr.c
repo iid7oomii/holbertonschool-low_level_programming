36efb9c1f9386a324fa3d8325ad07297ab0d8c9c
iid7oomii <iid7oomii>
2025-09-11 13:23:45 +0000

    sas

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c in s,
+ * or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
+}
