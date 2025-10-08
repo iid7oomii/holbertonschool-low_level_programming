0b87262129a8d01b2e419cfacfc3fd9b31ad579d
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-10-08 08:05:03 +0000

    complete task 0 with file name:0-print_list.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,63 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Function prototypes */
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h);
 
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of elements
+ */
 size_t list_len(const list_t *h);
 
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to a pointer to the head of the list
+ * @str: string to add in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str);
 
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to a pointer to the head of the list
+ * @str: string to add in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str);
 
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ */
 void free_list(list_t *head);
+
+#endif /* LISTS_H */
