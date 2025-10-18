ee0aee5efc9e286cc4ca92029a06b0af1948a25d
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-10-18 16:39:14 +0000

    Task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 
 /**
- * create_file - Creates a file with specified permissions and writes text to it.
- * @filename: The name of the file to create.
- * @text_content: A NULL terminated string to write to the file.
+ * create_file - Creates a file with permissions and writes text
+ * @filename: The name of the file to create
+ * @text_content: A NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
