44534079474e8a2c0efe612a49d465e731ede0e9
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-10-18 17:25:37 +0000

    task3

--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
+/* ===== Function Prototypes ===== */
+
+/* 0-read_textfile.c */
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
 
+/* 1-create_file.c */
+int create_file(const char *filename, char *text_content);
+
+/* 2-append_text_to_file.c */
+int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void close_file(int fd);
+char *create_buffer(char *file);
 #endif /* MAIN_H */
