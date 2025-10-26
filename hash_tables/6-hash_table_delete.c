48e2bc27ebcf6ddd104d04908b53e5f77cb7d49a
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-10-26 08:12:08 +0000

    TASK6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ *
+ * Description: Frees all memory allocated for the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp, *next;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			next = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
